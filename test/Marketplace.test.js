68ce95dec7dc964dc249858632bacbecdb542568
William Willems <william.willems@gmail.com>
2019-10-01 20:31:28 +0200

    added products and test coverage

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,6 +1,10 @@
 const Marketplace = artifacts.require('./Marketplace.sol')
 
-contract('Marketplace', (accounts) => {
+require('chai')
+	.use(require('chai-as-promised'))
+	.should()
+
+contract('Marketplace', ([deployer, seller, buyer]) => {
 	let marketplace
 
 	before(async () => {
@@ -17,6 +21,29 @@ contract('Marketplace', (accounts) => {
 		it('has the proper name', async() => {
 			const name = await marketplace.name()
 			assert.equal(name, 'Marketplace')
+		})
+	})
+
+	describe('products', async() => {
+		let result, productCount
+
+		before(async() => {
+			result = await marketplace.createProduct('iPhone X', web3.utils.toWei('1', 'Ether'), {from: seller})
+			productCount = await marketplace.productCount()
+		})
+
+		it('creates products', async() => {
+			assert.equal(productCount, 1)
+			const event = result.logs[0].args
+			assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+			assert.equal(event.name, 'iPhone X', 'name is correct')
+			assert.equal(event.price, '1000000000000000000', 'price is correct')
+			assert.equal(event.owner, seller, 'owner is correct')
+			assert.equal(event.purchased, false, 'purchased is correct')
+
+			await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), {from: seller}).should.be.rejected;
+			await marketplace.createProduct('iPhone X', 0, {from: seller}).should.be.rejected;
+		
 		})
 	})
 })
